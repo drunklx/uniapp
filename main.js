bd483282b2a1462d3423f1d3f57d6bfb82905426
lx <2439187581@qq.com>
2025-02-22 15:23:12 +0800

    完成了home首页的开发

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 
-// #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
 
+import { $http } from '@escook/request-miniprogram'
+uni.$http=$http
+// 请求拦截器
+$http.beforeRequest=function(options){
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+
+//请求根路径
+$http.baseUrl='https://api-hmugo-web.itheima.net'
+//响应拦截器
+$http.afterRequest=function(){
+	uni.hideLoading()
+}
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
+//封装弹框方法
+uni.$showMsg=function(title='数据请求失败',duration=1500)
+{
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
@@ -11,15 +42,3 @@ const app = new Vue({
     ...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-import App from './App.vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
